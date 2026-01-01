db4c8930d5b3b8134e9670a27f0d1fa7984ebfbb
Adedoyin Famuyiwa <adedoyinfamuyiwa@gmail.com>
2025-12-31 19:35:12 -0500

    test npm run script and actions

--- .generated/blogs/test-action.js
+++ .generated/blogs/test-action.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-const markdown = "test more actions\r\n";
+const markdown = "test more actions\r\n\r\nanother line here";
 
 export default function TestAction() {
   return <ReactMarkdown>{markdown}</ReactMarkdown>;
