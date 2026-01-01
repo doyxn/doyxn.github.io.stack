10cfa78d989be76eb86e0bd10fd923c68b4f9228
Adedoyin Famuyiwa <adedoyinfamuyiwa@gmail.com>
2026-01-01 09:18:02 -0500

    triggering another action

--- .generated/blogs/test-action.js
+++ .generated/blogs/test-action.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-const markdown = "\r\ntest more actions\r\nupdates again";
+const markdown = "\r\ntest more actions\r\nupdates again, last";
 
 export default function TestAction() {
   return <ReactMarkdown>{markdown}</ReactMarkdown>;
